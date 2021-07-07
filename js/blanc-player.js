0a989310e4ecd646721e0bbc58e8f648b183bde8
c9u11 <tjrdud6412@naver.com>
2021-07-07 22:28:19 +0900

    ADD : hotkey

--- js/blanc-player.js
+++ js/blanc-player.js
@@ -5,48 +5,95 @@ var blcplayer = (function () {
   function privateFunc() {
     console.log("private!");
   }
-  function addClass(el, className){
+  function addClass(el, className) {
     el.classList.add(className);
   }
-  function removeClass(el, className){
+  function removeClass(el, className) {
     el.classList.remove(className);
+  }
+  function hasClass(el, className) {
+    return el.classList.contains(className);
   }
   function makeElement(option) {
     var element = document.createElement(option.tag);
-    if(option.id){
+    if (option.id) {
       element.id = option.id || "";
     }
-    if(option.class){
+    if (option.class) {
       element.classList.add(option.class);
     }
     if (option.click) element.addEventListener("click", option.click);
     return element;
   }
   function removeElement(type, string, index = 0) {
-    switch (type){
-      case "id" :
-        if(isExistsElement("id", string))
+    switch (type) {
+      case "id":
+        if (isExistsElement("id", string))
           document.getElementById(string).remove();
-        return
-      case "class" :
-        if(isExistsElement("class",string,index))
-        document.getElementsByClassName(string)[index].remove();
-        return
-      default :
+        return;
+      case "class":
+        if (isExistsElement("class", string, index))
+          document.getElementsByClassName(string)[index].remove();
+        return;
+      default:
         throw new Error("Not defined type");
     }
   }
-  function isExistsElement(type, string, index = 0){
-    switch (type){
-      case "id" :
+  function isExistsElement(type, string, index = 0) {
+    switch (type) {
+      case "id":
         var element = document.getElementById(string);
         return typeof element != undefined && element != null;
-      case "class" :
-        var element = document.getElementsByClassName(string)[index]
-        return  typeof element != undefined && element != null;
-      default :
+      case "class":
+        var element = document.getElementsByClassName(string)[index];
+        return typeof element != undefined && element != null;
+      default:
         throw new Error("Not defined type");
     }
+  }
+  function addHotkeyEvent() {
+    window.onkeydown = function (e) {
+      return !(e.keyCode == 32);
+    };
+    blcplayer.player.setAttribute("tabindex", 0);
+    blcplayer.player.addEventListener("keydown", function (event) {
+      var player = blcplayer.player;
+      var video = blcplayer.video;
+      var option = blcplayer.option.hotkey;
+      switch (event.key) {
+        case "ArrowLeft":
+          if (!option.secSkip) return;
+          video.currentTime = video.currentTime - option.secSkip;
+          break;
+        case "ArrowRight":
+          if (!option.secSkip) return;
+          video.currentTime = video.currentTime + option.secSkip;
+          break;
+        case " ":
+          if (!option.playToggle) return;
+          if (video.paused) video.play();
+          else video.pause();
+          break;
+        case "m":
+          if (!option.soundToggle) return;
+          video.muted = !video.muted;
+          break;
+        case "w":
+          if (!option.wideToggle) return;
+          if (hasClass(player, "blc-wide-screen"))
+            removeClass(player, "blc-wide-screen");
+          else addClass(player, "blc-wide-screen");
+          break;
+        case "f":
+          if (!option.fullToggle) return;
+          if (hasClass(player, "blc-full-screen"))
+            removeClass(player, "blc-full-screen");
+          else addClass(player, "blc-full-screen");
+          break;
+        default:
+          break;
+      }
+    });
   }
   // 공개 Object
   return {
@@ -102,12 +149,16 @@ var blcplayer = (function () {
       hover: 3,
       // controlBar : true
       controlBar: true,
+      // hotkey : true
+      hotkey: {
+        secSkip: 5,
+        playToggle: true,
+        soundToggle: true,
+        wideToggle: true,
+        fullToggle: true,
+      },
       // 기본 PIP 모드 : false
       pictureInPictureToggle: false,
-      // 중앙 플레이 버튼 : false
-      bigPlayButton: false,
-      // n초 넘어가기 버튼 : 0
-      skipButton: 0,
       // pip mode : false
       pip: false,
       // 시네마 모드 : false
@@ -128,19 +179,19 @@ var blcplayer = (function () {
       toolTip: false,
     },
     // 공개 함수
-    init: function(customOption = {}, func) {
+    init: function (customOption = {}, func) {
       // option merge
       this.option = Object.assign(this.option, customOption);
 
       // blc-player 선언
-      this.player = document.getElementById('blc-player');
+      this.player = document.getElementById("blc-player");
 
       // Video tag 추가
-      this.player.innerHTML = `<video id="blc-video" controls></video>`;
-      this.video = document.getElementById('blc-video');
+      this.player.innerHTML = `<video id="blc-video"></video>`;
+      this.video = document.getElementById("blc-video");
 
       // src 추가
-      this.video.setAttribute('src',this.option.sources[0].src);
+      this.video.setAttribute("src", this.option.sources[0].src);
 
       // device class 추가
       this.player.classList.add(this.option.device);
@@ -149,8 +200,18 @@ var blcplayer = (function () {
       this.hover(this.option.hover);
       // controls 기능
       this.controlBar(this.option.controlBar);
-      // // skipButton 기능
-      // this.skipButton(this.option.skipButton);
+      // hotkey
+      addHotkeyEvent();
+      // secSkip 기능
+      this.hotkey.secSkip(this.option.hotkey.secSkip);
+      // playToggle
+      this.hotkey.playToggle(this.option.hotkey.playToggle);
+      // soundToggle
+      this.hotkey.soundToggle(this.option.hotkey.soundToggle);
+      // wideToggle
+      this.hotkey.wideToggle(this.option.hotkey.wideToggle);
+      // fullToggle
+      this.hotkey.fullToggle(this.option.hotkey.fullToggle);
       // // pip 기능
       // this.pip(this.option.pip);
       // // wideButton 기능
@@ -170,53 +231,53 @@ var blcplayer = (function () {
       // // toolTip
       // this.toolTip(this.option.toolTip);
       // func 실행
-      if(typeof func === "function") func();
+      if (typeof func === "function") func();
     },
-    hover: function(sec){
+    hover: function (sec) {
       // 현재 설정값 return
-      if(sec === undefined) return this.option.hover;
+      if (sec === undefined) return this.option.hover;
 
       // Parameter error
-      if(typeof sec !== "number") throw new Error("Parameter is not number");
+      if (typeof sec !== "number") throw new Error("Parameter is not number");
 
       // false 일 때 기능 제거
-      if(!sec) {
+      if (!sec) {
         // option 값 변경
         this.option.hover = sec;
         // 이벤트 제거
-        this.player.removeEventListener("mousemove",hover);
-      }
-      else {
+        this.player.removeEventListener("mousemove", hover);
+      } else {
         // option 값 변경
         this.option.hover = sec;
         // 이벤트 추가
-        this.player.addEventListener("mousemove",hover);
+        this.player.addEventListener("mousemove", hover);
       }
 
       // timeout object
       var outFunc = null;
       // mouseenter func
-      function hover(){
+      function hover() {
         // player class 추가
         addClass(this, "hover");
         // timeout clear
-        clearTimeout(outFunc)
+        clearTimeout(outFunc);
         // timeout set
-        outFunc = setTimeout(function(){
+        outFunc = setTimeout(function () {
           // player class 추가
-          removeClass(blcplayer.player,"hover");
-        },blcplayer.option.hover * 1000);
-      };
+          removeClass(blcplayer.player, "hover");
+        }, blcplayer.option.hover * 1000);
+      }
 
       return "hover : " + this.option.hover + "sec";
     },
-    controlBar: function(bool){
+    controlBar: function (bool) {
       // 현재 설정값 return
       if (bool === undefined) return this.option.controlBar;
-  
+
       // Parameter error
-      if (typeof bool !== "boolean") throw new Error("Parameter is not boolean");
-    
+      if (typeof bool !== "boolean")
+        throw new Error("Parameter is not boolean");
+
       // false 일 때 기능 제거
       if (!bool) {
         // option 값 변경
@@ -231,16 +292,95 @@ var blcplayer = (function () {
         // player class 추가
         addClass(this.player, "blc-control-bar");
         /* Big Play 버튼 */
-        if(!isExistsElement("id", "blc-control-bar")){
+        if (!isExistsElement("id", "blc-control-bar")) {
           var controlBar = makeElement({
             tag: "div",
-            id: "blc-control-bar"
+            id: "blc-control-bar",
           });
           this.player.appendChild(controlBar);
         }
       }
-  
+
       return "controlBar : " + this.option.controlBar;
-    }
+    },
+    hotkey: {
+      toggle: function (bool) {
+        // 현재 설정값 return
+        if (bool === undefined) return blcplayer.option.hotkey;
+
+        // Parameter error
+        if (typeof bool !== "boolean")
+          throw new Error("Parameter is not boolean");
+
+        // option 값 변경
+        blcplayer.option.hotkey.secSkip = bool;
+
+        return "hotkey - Toggle : " + bool;
+      },
+      secSkip: function (sec) {
+        // 현재 설정값 return
+        if (sec === undefined) return blcplayer.option.hotkey.secSkip;
+
+        // Parameter error
+        if (typeof sec !== "number") throw new Error("Parameter is not Number");
+
+        // option 값 변경
+        blcplayer.option.hotkey.secSkip = sec;
+
+        return "hotkey - secSkip Value : " + blcplayer.option.hotkey.secSkip;
+      },
+      playToggle: function (bool) {
+        // 현재 설정값 return
+        if (bool === undefined) return blcplayer.option.hotkey.playToggle;
+
+        // Parameter error
+        if (typeof bool !== "boolean")
+          throw new Error("Parameter is not boolean");
+
+        // option 값 변경
+        blcplayer.option.hotkey.playToggle = bool;
+
+        return "hotkey - playToggle : " + blcplayer.option.hotkey.playToggle;
+      },
+      soundToggle: function (bool) {
+        // 현재 설정값 return
+        if (bool === undefined) return blcplayer.option.hotkey.soundToggle;
+
+        // Parameter error
+        if (typeof bool !== "boolean")
+          throw new Error("Parameter is not boolean");
+
+        // option 값 변경
+        blcplayer.option.hotkey.soundToggle = bool;
+
+        return "hotkey - soundToggle : " + blcplayer.option.hotkey.soundToggle;
+      },
+      wideToggle: function (bool) {
+        // 현재 설정값 return
+        if (bool === undefined) return blcplayer.option.hotkey.wideToggle;
+
+        // Parameter error
+        if (typeof bool !== "boolean")
+          throw new Error("Parameter is not boolean");
+
+        // option 값 변경
+        blcplayer.option.hotkey.wideToggle = bool;
+
+        return "hotkey - wideToggle : " + blcplayer.option.hotkey.wideToggle;
+      },
+      fullToggle: function (bool) {
+        // 현재 설정값 return
+        if (bool === undefined) return blcplayer.option.hotkey.fullToggle;
+
+        // Parameter error
+        if (typeof bool !== "boolean")
+          throw new Error("Parameter is not boolean");
+
+        // option 값 변경
+        blcplayer.option.hotkey.fullToggle = bool;
+
+        return "hotkey - fullToggle : " + blcplayer.option.hotkey.fullToggle;
+      },
+    },
   };
 })();
